aba774ffcbfb0bef4f69d8fadb50b95c15754f45
Stephen Green <SGreen@qicglobal.com>
2020-11-10 19:02:27 +0000

    basics created in Nx app

--- SPA/apps/app/src/app/app.module.ts
+++ SPA/apps/app/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { CreateDataComponent } from './create-data/create-data.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NavMenuComponent, CreateDataComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
