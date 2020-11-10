e3fd221d0923dd7e8200ff054b904f997f6b63b3
Stephen Green <SGreen@qicglobal.com>
2020-11-10 15:06:31 +0000

    Workspace created with Nx

--- SPA/src/app/domain/models/something-else.ts
+++
@@ -1,8 +0,0 @@
-import { Something } from './something';
-
-export interface SomethingElse {
-  name: string;
-  somethings: Something[];
-  id: number;
-  tag: string;
-}
--- SPA/src/app/domain/models/something.ts
+++
@@ -1,4 +0,0 @@
-export interface Something {
-  name: string;
-  id: number;
-}
